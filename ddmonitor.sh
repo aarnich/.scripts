3e22fbe700bb4727dcde332d3e3402f3061c65e8
aarnich <aarn.gmpc@gmail.com>
2022-03-26 18:32:21 +0800

    changed ddmonitor to backup configs before cp command

--- ddmonitor.sh
+++ ddmonitor.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 STATE=$(xrandr --listproviders | head -1 | grep -Po "\\d+")
 if [[ $STATE = "2" ]]; then
+
+  sudo cp /etc/X11/xorg.conf /etc/X11/xorg.conf.PRIMEBACKUP
   sudo cp /etc/X11/xorg.conf.backup /etc/X11/xorg.conf
   sudo systemctl restart display-manager
 fi
 if [[ $STATE = "1" ]]; then
+  sudo cp /etc/X11/xorg.conf /etc/X11/xorg.conf.backup
   sudo cp /etc/X11/xorg.conf.PRIMEBACKUP /etc/X11/xorg.conf
   sudo systemctl restart display-manager
 fi
